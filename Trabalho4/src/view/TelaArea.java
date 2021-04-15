fe2a573ac604983efb735c2560069922cc6d8e6b
Alladin-51 <igortls20@gmail.com>
2021-04-15 18:09:40 -0300

    Tela Area atualizada

--- Trabalho4/src/view/TelaArea.java
+++ Trabalho4/src/view/TelaArea.java
@@ -20,42 +20,55 @@ public class TelaArea extends JFrame {
 	private JTextField fieldArea;
 
 	private JPanel contentPane;
+	private JTextField fieldResultadoArea;
 
 	public TelaArea() {
 		
 		Area area = new Area();
 		
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 100, 400, 350);
 		getContentPane().setLayout(null);
 		
 		JLabel lblTextoArea1 = new JLabel("Digite o valor a ser convertido");
-		lblTextoArea1.setBounds(128, 11, 209, 14);
+		lblTextoArea1.setBounds(108, 11, 220, 14);
 		getContentPane().add(lblTextoArea1);
 		
 		fieldArea = new JTextField();
-		fieldArea.setBounds(159, 36, 86, 20);
+		fieldArea.setBounds(138, 36, 86, 20);
 		getContentPane().add(fieldArea);
 		fieldArea.setColumns(10);
 		
 		JLabel lblTextoArea2 = new JLabel("Selecione a unidade de medida do numero digitado acima");
-		lblTextoArea2.setBounds(65, 67, 333, 14);
+		lblTextoArea2.setBounds(29, 67, 331, 14);
 		getContentPane().add(lblTextoArea2);
 		
 		JComboBox comboArea1 = new JComboBox();
 		comboArea1.setModel(new DefaultComboBoxModel(new String[] {"Milimetro quadrado", "Centimetro quadrado", "Decimetro quadrado", "Metro quadrado", "Quilometro quadrado", "Milha quadrada", "Pes quadrados"}));
-		comboArea1.setBounds(128, 92, 144, 22);
+		comboArea1.setBounds(122, 92, 130, 22);
 		getContentPane().add(comboArea1);
 		
 		JLabel lblTextoArea3 = new JLabel("Selecione a unidade para qual deseja converter");
-		lblTextoArea3.setBounds(97, 125, 286, 14);
+		lblTextoArea3.setBounds(51, 123, 295, 14);
 		getContentPane().add(lblTextoArea3);
 		
 		JComboBox comboArea2 = new JComboBox();
 		comboArea2.setModel(new DefaultComboBoxModel(new String[] {"Milimetro quadrado", "Centimetro quadrado", "Decimetro quadrado", "Metro quadrado", "Quilometro quadrado", "Milha quadrada", "Pes quadrados"}));
-		comboArea2.setBounds(128, 150, 144, 22);
+		comboArea2.setBounds(122, 150, 130, 22);
 		getContentPane().add(comboArea2);
 		
-		JButton btnBotaoArea = new JButton("OK");
+		fieldResultadoArea = new JTextField();
+		fieldResultadoArea.setBounds(142, 280, 86, 20);
+		getContentPane().add(fieldResultadoArea);
+		fieldResultadoArea.setColumns(10);
+		fieldResultadoArea.setVisible(false);
+		
+		JLabel lblTextoArea4 = new JLabel("Resultado");
+		lblTextoArea4.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblTextoArea4.setBounds(149, 255, 79, 14);
+		getContentPane().add(lblTextoArea4);
+		lblTextoArea4.setVisible(false);
+		
+		JButton btnBotaoArea = new JButton("Converter");
 		btnBotaoArea.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -65,23 +78,30 @@ public class TelaArea extends JFrame {
 				area.converteArea(valor, comboArea1.getSelectedIndex());
 			
 				// Exibição do resultado
-				JFrame frame = new JFrame("Resultado");
-				frame.setSize(150, 150);
-				JLabel labelT = new JLabel(area.getArea(comboArea2.getSelectedIndex()));
-				labelT.setFont( new Font("Arial", Font.PLAIN, 20));
-			//	contentPane.add(labelT);
-				labelT.setBounds(50, 50, 50, 50);
-				frame.getContentPane().add(labelT);
-				frame.setVisible(true);
-				setLocationRelativeTo(null);
-				setVisible(true);
-			
 				
+				lblTextoArea4.setVisible(true);
+				fieldResultadoArea.setVisible(true);
+				fieldResultadoArea.setText(area.getArea(comboArea2.getSelectedIndex()));
 				
 			}
 		});
-		btnBotaoArea.setBounds(159, 193, 89, 23);
+		btnBotaoArea.setBounds(132, 193, 113, 23);
 		getContentPane().add(btnBotaoArea);
+		
+		JButton btnBotaoVoltarArea = new JButton("Voltar");
+		btnBotaoVoltarArea.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				setVisible(false);
+				new TelaPrincipal().setVisible(true);
+			}
+		});
+		btnBotaoVoltarArea.setBounds(10, 279, 89, 23);
+		getContentPane().add(btnBotaoVoltarArea);
+		
+		
+		
+		
 		setLocationRelativeTo(null);
 		setVisible(true);
 		
