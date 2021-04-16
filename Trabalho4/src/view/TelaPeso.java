3652fe17dda992196bde95b4e3c3c198425090c0
Alladin-51 <igortls20@gmail.com>
2021-04-16 00:25:23 -0300

    Tela Area atualizada

--- Trabalho4/src/view/TelaPeso.java
+++ Trabalho4/src/view/TelaPeso.java
@@ -21,44 +21,56 @@ public class TelaPeso extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField fieldPeso;
+	private JTextField fieldResultadoPeso;
 
 	public TelaPeso() {
 		
 		
 		Peso peso = new Peso();
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 100, 400, 350);
 		getContentPane().setLayout(null);
 		
 		
 		JLabel lblTextoPeso1 = new JLabel("Digite o valor a ser convertido");
-		lblTextoPeso1.setBounds(128, 11, 195, 14);
+		lblTextoPeso1.setBounds(108, 11, 220, 14);
 		getContentPane().add(lblTextoPeso1);
 		
 		fieldPeso = new JTextField();
-		fieldPeso.setBounds(157, 36, 86, 20);
+		fieldPeso.setBounds(138, 36, 86, 20);
 		getContentPane().add(fieldPeso);
 		fieldPeso.setColumns(10);
 		
 		JLabel lblTextoPeso2 = new JLabel("Selecione a unidade de medida do numero digitado acima");
-		lblTextoPeso2.setBounds(70, 67, 332, 14);
+		lblTextoPeso2.setBounds(29, 67, 331, 14);
 		getContentPane().add(lblTextoPeso2);
 		
 		JComboBox comboPeso1 = new JComboBox();
 		comboPeso1.setModel(new DefaultComboBoxModel(new String[] {"Miligrama", "Grama", "Quilograma", "Libras", "Toneladas"}));
-		comboPeso1.setBounds(145, 92, 115, 22);
+		comboPeso1.setBounds(122, 92, 130, 22);
 		getContentPane().add(comboPeso1);
 		
 		JLabel lblTextoPeso3 = new JLabel("Selecione a unidade para qual deseja converter");
-		lblTextoPeso3.setBounds(95, 125, 277, 14);
+		lblTextoPeso3.setBounds(51, 123, 295, 14);
 		getContentPane().add(lblTextoPeso3);
 		
 		JComboBox comboPeso2 = new JComboBox();
 		comboPeso2.setModel(new DefaultComboBoxModel(new String[] {"Miligrama", "Grama", "Quilograma", "Libras", "Toneladas"}));
-		comboPeso2.setBounds(145, 150, 115, 22);
+		comboPeso2.setBounds(122, 150, 130, 22);
 		getContentPane().add(comboPeso2);
 		
+		fieldResultadoPeso = new JTextField();
+		fieldResultadoPeso.setBounds(142, 280, 86, 20);
+		getContentPane().add(fieldResultadoPeso);
+		fieldResultadoPeso.setColumns(10);
+		fieldResultadoPeso.setVisible(false);
 		
-		JButton btnBotaoPeso = new JButton("OK");
+		JLabel lblTextoPeso4 = new JLabel("Resultado");
+		lblTextoPeso4.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblTextoPeso4.setBounds(149, 255, 79, 14);
+		getContentPane().add(lblTextoPeso4);
+		lblTextoPeso4.setVisible(false);
+		
+		JButton btnBotaoPeso = new JButton("Converter");
 		btnBotaoPeso.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				double valor = Double.parseDouble(fieldPeso.getText().toString());
@@ -67,23 +79,32 @@ public class TelaPeso extends JFrame {
 				peso.convertePeso(valor, comboPeso1.getSelectedIndex());
 			
 				// Exibição do resultado
-				JFrame frame = new JFrame("Resultado");
-				frame.setSize(150, 150);
-				JLabel labelT = new JLabel(peso.getPeso(comboPeso2.getSelectedIndex()));
-				labelT.setFont( new Font("Arial", Font.PLAIN, 20));
-				labelT.setBounds(100, 70, 150, 150);
-				frame.getContentPane().add(labelT);
-				frame.setVisible(true);
-				setLocationRelativeTo(null);
-				setVisible(true);
 				
+				lblTextoPeso4.setVisible(true);
+				fieldResultadoPeso.setVisible(true);
+				fieldResultadoPeso.setText(peso.getPeso(comboPeso2.getSelectedIndex()));
 				
 				
 				
 			}
 		});
-		btnBotaoPeso.setBounds(154, 183, 89, 23);
+		btnBotaoPeso.setBounds(132, 193, 113, 23);
 		getContentPane().add(btnBotaoPeso);
+		
+		JButton btnBotaoVoltarPeso = new JButton("Voltar");
+		btnBotaoVoltarPeso.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				setVisible(false);
+				new TelaPrincipal().setVisible(true);
+			}
+		});
+		btnBotaoVoltarPeso.setBounds(10, 279, 89, 23);
+		getContentPane().add(btnBotaoVoltarPeso);
+		
+		
+		
+		
 		setLocationRelativeTo(null);
 		setVisible(true);
 		
