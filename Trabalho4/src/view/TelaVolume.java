3702301c37fab8415b05a9e17385c77b3eaddbbb
Alladin-51 <igortls20@gmail.com>
2021-04-16 01:01:18 -0300

    Tela volume atualizada

--- Trabalho4/src/view/TelaVolume.java
+++ Trabalho4/src/view/TelaVolume.java
@@ -16,42 +16,56 @@ import java.awt.event.ActionEvent;
 
 public class TelaVolume extends JFrame {
 	private JTextField fieldVol;
+	private JTextField fieldResultadoVol;
 
 	public TelaVolume() {
 		
 		Volume volume = new Volume();
 		
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 100, 400, 350);
 		getContentPane().setLayout(null);
 		
 		JLabel lblTextoVol1 = new JLabel("Digite o valor a ser convertido");
-		lblTextoVol1.setBounds(128, 11, 214, 14);
+		lblTextoVol1.setBounds(108, 11, 220, 14);
 		getContentPane().add(lblTextoVol1);
 		
 		fieldVol = new JTextField();
-		fieldVol.setBounds(159, 36, 86, 20);
+		fieldVol.setBounds(138, 36, 86, 20);
 		getContentPane().add(fieldVol);
 		fieldVol.setColumns(10);
 		
 		JLabel lblTextoVol2 = new JLabel("Selecione a unidade de medida do numero digitado acima");
-		lblTextoVol2.setBounds(70, 67, 314, 14);
+		lblTextoVol2.setBounds(29, 67, 331, 14);
 		getContentPane().add(lblTextoVol2);
 		
 		JComboBox comboVol1 = new JComboBox();
 		comboVol1.setModel(new DefaultComboBoxModel(new String[] {"Mililitros", "Litros", "Milimetros cubicos", "Centimetros cubicos", "Decimetros cubicos", "Metros cubicos", "Pes cubicos"}));
-		comboVol1.setBounds(142, 92, 126, 22);
+		comboVol1.setBounds(122, 92, 130, 22);
 		getContentPane().add(comboVol1);
 		
 		JLabel lblTextoVol3 = new JLabel("Selecione a unidade para qual deseja converter");
-		lblTextoVol3.setBounds(94, 125, 290, 14);
+		lblTextoVol3.setBounds(51, 123, 295, 14);
 		getContentPane().add(lblTextoVol3);
 		
 		JComboBox comboVol2 = new JComboBox();
 		comboVol2.setModel(new DefaultComboBoxModel(new String[] {"Mililitros", "Litros", "Milimetros cubicos", "Centimetros cubicos", "Decimetros cubicos", "Metros cubicos", "Pes cubicos"}));
-		comboVol2.setBounds(142, 150, 130, 22);
+		comboVol2.setBounds(122, 150, 130, 22);
 		getContentPane().add(comboVol2);
 		
-		JButton btnBotaoVol = new JButton("OK");
+		fieldResultadoVol = new JTextField();
+		fieldResultadoVol.setBounds(142, 280, 86, 20);
+		getContentPane().add(fieldResultadoVol);
+		fieldResultadoVol.setColumns(10);
+		fieldResultadoVol.setVisible(false);
+		
+		JLabel lblTextoVol4 = new JLabel("Resultado");
+		lblTextoVol4.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblTextoVol4.setBounds(149, 255, 79, 14);
+		getContentPane().add(lblTextoVol4);
+		lblTextoVol4.setVisible(false);
+		
+		
+		JButton btnBotaoVol = new JButton("Converter");
 		btnBotaoVol.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -61,20 +75,31 @@ public class TelaVolume extends JFrame {
 				volume.converteVol(valor, comboVol1.getSelectedIndex());
 			
 				// Exibição do resultado
-				JFrame frame = new JFrame("Resultado");
-				frame.setSize(150, 150);
-				JLabel labelT = new JLabel(volume.getVol(comboVol2.getSelectedIndex()));
-				labelT.setFont( new Font("Arial", Font.PLAIN, 20));
-				labelT.setBounds(100, 70, 150, 150);
-				frame.add(labelT);
-				frame.setVisible(true);
-				setLocationRelativeTo(null);
-				setVisible(true);
+				
+				lblTextoVol4.setVisible(true);
+				fieldResultadoVol.setVisible(true);
+				fieldResultadoVol.setText(volume.getVol(comboVol2.getSelectedIndex()));
 				
 			}
 		});
-		btnBotaoVol.setBounds(156, 195, 89, 23);
+		btnBotaoVol.setBounds(132, 193, 113, 23);
 		getContentPane().add(btnBotaoVol);
+		
+		JButton btnBotaoVoltarVol = new JButton("Voltar");
+		btnBotaoVoltarVol.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				setVisible(false);
+				new TelaPrincipal().setVisible(true);
+				
+			}
+		});
+		btnBotaoVoltarVol.setBounds(10, 279, 89, 23);
+		getContentPane().add(btnBotaoVoltarVol);
+		
+		
+		
+		
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
